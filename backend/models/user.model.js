bed74cba242dc6216fd22ed4a7adaf44a57a64f3
Willem April <willemapril33@gmail.com>
2025-10-03 16:04:34 +0200

    Add User model with schema definition
    Introduces a Mongoose schema for the User model, including fields for name, email, password, and role, with appropriate validation and defaults. The schema also enables timestamps for created and updated records.

---
+++ backend/models/user.model.js
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, { timestamps: true });
+
+export const User = mongoose.model('User', userSchema);
